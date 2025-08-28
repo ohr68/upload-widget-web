b5dce57c1346850a772f1d40fe52b2a404876b33
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-08-27 21:24:19 -0300

    feat: :sparkles: Adiciona listagem de arquivos e componente de drop zone

--- src/components/upload-widget-title.tsx
+++ src/components/upload-widget-title.tsx
@@ -1,5 +1,10 @@
+import { UploadCloud } from 'lucide-react'
+
 export function UploadWidgetTitle () {
   return (
-    <div>teste</div>
+    <div className='flex items-center gap-1.5 text-sm font-medium'>
+      <UploadCloud className='size-4 text-zinc-400' strokeWidth={1.5} />
+      <span className='text-sm font-medium'>Upload files</span>
+    </div>
   )
 }
